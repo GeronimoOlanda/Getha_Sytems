4fe223f27c99bba73b33eff419363073a95a040e
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-06-22 00:16:59 -0300

    criação da listaDTO

---
+++ Geta_Systems.API.Entities/DTOs/ListaDTODados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geta_Systems.API.Entities.DTOs
+{
+    public class ListaDTODados
+    {
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+    }
+}
